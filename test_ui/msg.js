6fa3a8dc405394a1587fc757ba44f7209012a7de
stella-ki <neatouo@naver.com>
2020-05-25 15:09:29 +0900

    Update msg.js

--- test_ui/msg.js
+++ test_ui/msg.js
@@ -14,18 +14,47 @@ function sendExtMessageCert() {
       });
 }
 
+function addOption(){
+  var objSel = document.getElementById("selbox");
+  for (i=0; i
+    var objOption = document.createElement("option");       
+    objOption.text = valArr[i] + "개";
+    objOption.value = valArr[i];
+   
+    objSel.options.add(objOption);
+  }
+}
+
+function removeOption(){
+  var objSel = document.getElementById("selbox");
+
+  //alert(objSel.options[i].text +"//"+ objSel.options[i].value +"//"+ i);
+  for(i=objSel.length; i>=0; i--){
+    objSel.options[i]=null;
+  }
+
+}
+
 function sendExtMessageSign() {
     chrome.runtime.sendMessage(extId, {type: "sign", content: "<signedingoajdkasd>", thumbprint: "c914cafe6b7ec2d01a0c709ec4e7a4afa0081e67"},
       function(response) {
         
         console.log(response);
+        removeOption();
+		
+		var result = '';
+		var list = response.certificates;
+        for(idx in array){
+			result += array[idx] + '</br>';
+		};
+
         if (!response.success)
           console.log("did not work");
 
         if (response.success)
           alert('ok');
 
-        document.getElementById('response').innerHTML += "<p>" + JSON.stringify(response) + "</p>";
+        document.getElementById('response').innerHTML += "<p>" + result + "</p>";
       });
 }
 
